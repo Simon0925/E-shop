d615767cdd18a997102e59c8e82ad5540bdc355b
Sam <49652580+Simon0925@users.noreply.github.com>
2025-02-23 19:51:42 +0000

    first commit

--- src/app/components/AplicationForm/services/sendForrm.ts
+++ src/app/components/AplicationForm/services/sendForrm.ts
@@ -1,14 +1,17 @@
-
-
-const sendForm = (prevState: any, formData: FormData) => {
-    const dataToSend = {
+interface FormState {
+    name: string;
+    phoneNumber: string;
+  }
+  
+  const sendForm = (prevState: FormState, formData: FormData): FormState => {
+    const dataToSend: FormState = {
       name: formData.get("name") as string,
-      phoneNumber: formData.get("phoneNumber") as string, 
+      phoneNumber: formData.get("user_phone") as string, 
     };
   
     console.log("formData", dataToSend);
-    
-    return dataToSend; 
+  
+    return dataToSend;
   };
   
   export default sendForm;
